1790554fb2c89e73dc85dcf5a3185c5503a194d6
jeremydiot <diot.jeremy@gmail.com>
2019-05-02 13:44:08 +0200

    add table managers

---
+++ src/managers/PostDbManager.java
@@ -0,0 +1,14 @@
+package managers;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class PostDbManager extends DbManager {
+	
+    //Constructors
+    public PostDbManager() {
+    	super();
+    	
+    }
+
+}
---
+++ src/managers/TopicDbManager.java
@@ -0,0 +1,13 @@
+package managers;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class TopicDbManager extends DbManager{
+	
+    //Constructors
+    public TopicDbManager() {
+    	super();
+    }
+
+}
